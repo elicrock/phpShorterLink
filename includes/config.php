a7c1e07e9a6b9a3fc59efbb3189d56ed27abb77a
Paul <paul.ivanov1990@gmail.com>
2021-10-26 12:58:38 +0300

    Homework 1

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('SITE_NAME', 'Cut your URL');
+define('HOST', 'http://' . $_SERVER['HTTP_HOST'] . '/phpShorterLink'); // сделано так, потому что все проекты делаются в одной главной папке
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'php_shorterlink');
+define('DB_USER', 'root');
+define('DB_PASS', '');
